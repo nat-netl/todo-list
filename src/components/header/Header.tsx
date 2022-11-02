e6445cf1495b200581f7c853935c0b25532d008c
nat-netl <nat_netl3@bk.ru>
2022-11-02 16:10:07 +0300

    header styles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,9 +3,9 @@ import s from './header.module.scss';
 
 const Header: FC = () => {
   return (
-    <div className={s.wrapper}>
+    <header className={s.wrapper}>
       <h1 className={s.title}>todos</h1>
-    </div>
+    </header>
   );
 };
 
